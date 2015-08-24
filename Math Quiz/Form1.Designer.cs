f9d27f68ccf13d1a7df538087218ee59a83cd809
Justin Harrison <justin.t.harrison@gmail.com>
2015-08-24 14:54:53 -0500

    Full

---
+++ Math Quiz/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Math_Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.plusLeftLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.plusRightLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.NumericUpDown();
+            this.difference = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.minusRightLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.minusLeftLabel = new System.Windows.Forms.Label();
+            this.quotient = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dividedRightLabel = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dividedLeftLabel = new System.Windows.Forms.Label();
+            this.product = new System.Windows.Forms.NumericUpDown();
+            this.label12 = new System.Windows.Forms.Label();
+            this.timesRightLabel = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.timesLeftLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difference)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            resources.ApplyResources(this.timeLabel, "timeLabel");
+            this.timeLabel.Name = "timeLabel";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // plusLeftLabel
+            // 
+            resources.ApplyResources(this.plusLeftLabel, "plusLeftLabel");
+            this.plusLeftLabel.Name = "plusLeftLabel";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // plusRightLabel
+            // 
+            resources.ApplyResources(this.plusRightLabel, "plusRightLabel");
+            this.plusRightLabel.Name = "plusRightLabel";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // sum
+            // 
+            resources.ApplyResources(this.sum, "sum");
+            this.sum.Name = "sum";
+            this.sum.Enter += new System.EventHandler(this.answer_Enter);
+            // 
+            // difference
+            // 
+            resources.ApplyResources(this.difference, "difference");
+            this.difference.Name = "difference";
+            this.difference.Enter += new System.EventHandler(this.answer_Enter);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // minusRightLabel
+            // 
+            resources.ApplyResources(this.minusRightLabel, "minusRightLabel");
+            this.minusRightLabel.Name = "minusRightLabel";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // minusLeftLabel
+            // 
+            resources.ApplyResources(this.minusLeftLabel, "minusLeftLabel");
+            this.minusLeftLabel.Name = "minusLeftLabel";
+            // 
+            // quotient
+            // 
+            resources.ApplyResources(this.quotient, "quotient");
+            this.quotient.Name = "quotient";
+            this.quotient.Enter += new System.EventHandler(this.answer_Enter);
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // dividedRightLabel
+            // 
+            resources.ApplyResources(this.dividedRightLabel, "dividedRightLabel");
+            this.dividedRightLabel.Name = "dividedRightLabel";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // dividedLeftLabel
+            // 
+            resources.ApplyResources(this.dividedLeftLabel, "dividedLeftLabel");
+            this.dividedLeftLabel.Name = "dividedLeftLabel";
+            // 
+            // product
+            // 
+            resources.ApplyResources(this.product, "product");
+            this.product.Name = "product";
+            this.product.Enter += new System.EventHandler(this.answer_Enter);
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // timesRightLabel
+            // 
+            resources.ApplyResources(this.timesRightLabel, "timesRightLabel");
+            this.timesRightLabel.Name = "timesRightLabel";
+            // 
+            // label14
+            // 
+            resources.ApplyResources(this.label14, "label14");
+            this.label14.Name = "label14";
+            // 
+            // timesLeftLabel
+            // 
+            resources.ApplyResources(this.timesLeftLabel, "timesLeftLabel");
+            this.timesLeftLabel.Name = "timesLeftLabel";
+            // 
+            // startButton
+            // 
+            resources.ApplyResources(this.startButton, "startButton");
+            this.startButton.Name = "startButton";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.quotient);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dividedRightLabel);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dividedLeftLabel);
+            this.Controls.Add(this.product);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.timesRightLabel);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.timesLeftLabel);
+            this.Controls.Add(this.difference);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.minusRightLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.minusLeftLabel);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.plusRightLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.plusLeftLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difference)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label plusLeftLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label plusRightLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown sum;
+        private System.Windows.Forms.NumericUpDown difference;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label minusRightLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label minusLeftLabel;
+        private System.Windows.Forms.NumericUpDown quotient;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label dividedRightLabel;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label dividedLeftLabel;
+        private System.Windows.Forms.NumericUpDown product;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label timesRightLabel;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label timesLeftLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Math Quiz/Form1.cs
@@ -0,0 +1,155 @@
+﻿//Please note: This application is purely for my own education, to run through coding 
+//examples by following tutorials, and to just tinker around with coding.  
+//I know it’s bad practice to heavily comment code (code smell), but comments in all of my 
+//exercises will largely be left intact as this serves me 2 purposes:
+//    I want to retain what my original thought process was at the time
+//    I want to be able to look back in 1..5..10 years to see how far I’ve come
+//    And I enjoy commenting on things, however redundant this may be . . . 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Math_Quiz
+{
+    public partial class Form1 : Form
+    {
+        // Create a random object to generate random numbers
+        Random randomizer = new Random();
+
+        // Integer variables to store the numbers for the addition problems
+        private int addend1;
+        private int addend2;
+
+        // Integer variables to store the numbers for the subtraction problems
+        private int minuend;
+        private int subtrahend;
+
+        // Integer variables to store the numbers for the multiplicatin problems
+        private int multiplicand;
+        private int multiplier;
+
+        // Integer variables to store the numbers for the division problems
+        private int dividend;
+        private int divisor;
+
+        // Variable to keep track of the time remaining
+        private int timeLeft;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void StartTheQuiz()
+        {
+            // The Addition line
+            // Varables addend1 and addend2 get assigned random values from 0-50
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+            // The value held in the new addend variables above are converted to strings to populate the form
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            // Initializes the result of the formula to 0
+            sum.Value = 0;
+
+            // The subtraction line
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            // The multiplication line
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            // The division line
+            divisor = randomizer.Next(2, 11);
+            // Using a method to ensure that the divedend is always a multiplier of the divisor for whole numbers
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+            // Start the timer
+            timeLeft = 30;
+            timeLabel.Text = timeLeft + " seconds";
+            timer1.Start();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+            timeLabel.BackColor = SystemColors.Control;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                // Checks for all the correct answers and if true congratulates the user
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!", "Gratz!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                // If the time is greater than 0, than the clock will count down and display in the timer box
+                // same as:
+                // timeLeft = timeLeft - 1;
+                timeLeft--;
+                timeLabel.Text = timeLeft + " seconds";
+                // color.control
+
+                if (timeLeft < 6)
+                    timeLabel.BackColor = Color.Red;
+            }
+            else
+            {
+                // This executes once the clock hits 0, shows a message and displays the answers
+                timer1.Stop();
+                timeLabel.Text = "Time's Up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+            }
+        }
+
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else return false;
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            // Select the whole answer in the NumericUpDown control
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lenghOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lenghOfAnswer);
+            }
+        }
+    }
+}
